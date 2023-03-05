0789eadee8e421bb24b98a6a856eb3a5f643ce3a
Alexwindy <72089138+alexwindy0@users.noreply.github.com>
2023-03-05 13:42:57 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends React.Component{
+    state ={word:''};
+
+    //using ComponentDidMount lifecycle method to fetch the API
+    componentDidMount(){
+        this.fetchWord();
+
+    }
+    
+    fetchWord = () =>{
+        //using axios to fetch the random advice api
+        axios.get('https://api.adviceslip.com/advice')
+        .then((response) =>{
+            const {advice} = response.data.slip;
+
+            //basic point javascript: 
+            //when a property and value have thesanme name. omit the later {advice:advice}
+            //but in this case our state is "word"
+            this.setState({word:advice})
+        })
+        .catch((error) =>{
+            console.log(error)
+        });
+    }
+
+    render(){
+        const {word} = this.state;
+        return(
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{word}</h1>
+                    <button className="button" onClick={this.fetchWord}>
+                        <span>Inspire Me!</span>
+                    </button>
+                </div>
+            
+            </div>
+            
+        )
+    }
 }
 
 export default App;
